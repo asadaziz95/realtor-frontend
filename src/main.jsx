9cdbfd2652e23e44119ecb62c5d50429a3089d2b
Asad Aziz <9146069+asadaziz95@users.noreply.github.com>
2025-02-01 14:10:49 +0500

    changes in route

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
  <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<App />} />
+      <Route path="/" element={<App />} />
       <Route
         path="/dashboard"
         element={
